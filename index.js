758e928918639505646af035cd7dd427b08d9b95
oxford026 <153386275+oxford026@users.noreply.github.com>
2025-03-09 01:39:26 +0300

    update

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 
 const addBtn = document.querySelector(`#btn1`);
 const deleteBtn = document.querySelector(`#btn2`)
+const summary = document.querySelector(`#summary`);
+const todostext = document.querySelector(`#input`);
+const hidecompleted = document.querySelector(`#HideCompleted`);
+
+const paragraphsCard = document.querySelector(`#paragraphsCard`);
+
+
 
 addBtn.addEventListener(`click`, function (e) {
 
     const text = document.querySelector(`#input`);
     console.log(text.value);
-    const p = document.createElement(`p`);
-    p.textContent = text.value;
-    document.querySelector(`body`).appendChild(p);
+    
+    const paragraphs = document.createElement(`p`);
+    paragraphs.textContent = text.value;
+    paragraphsCard.appendChild(paragraphs);
     text.value = ``;
+
     const checkbox = document.createElement(`input`);
     checkbox.type = `checkbox`;
-    p.appendChild(checkbox);
+    paragraphs.appendChild(checkbox);
 
     const updateSummary = function () {
-        const checkboxes = Array.from(document.querySelectorAll(`input[type="checkbox"]`));
+        const checkboxes = Array.from(document.querySelectorAll(`p input[type="checkbox"]`));
         const incompleteTodos = checkboxes.filter(function (checkbox) {
             return !checkbox.checked;
         });
-        let summary = document.querySelector(`#summary`);
+        
 
         if (!summary) {
             summary = document.createElement(`h3`);
             document.querySelector(`body`).appendChild(summary);
         }
-
 
         summary.textContent = `You have ${incompleteTodos.length} todos left.`;
     };
@@ -34,22 +42,31 @@ addBtn.addEventListener(`click`, function (e) {
     updateSummary();
 });
 
-
 deleteBtn.addEventListener(`click`, function (e) {
-    const isChecked = document.querySelectorAll(`input[type="checkbox"]:checked`);
+    const isChecked = document.querySelectorAll(`p input[type="checkbox"]:checked`);
     isChecked.forEach(function (check) {
         check.parentElement.remove();
 
-        const isCompleted = document.querySelectorAll(`input[type="checkbox"]`).length === 0;
-        if (isCompleted) {
+        const isCompleted = document.querySelectorAll(`p input[type="checkbox"]`).length === 0;
+        if (isCompleted) {          
             summary.textContent = `Congratulations! You have completed all your todos!`;
 
         }
-
     });
+});
 
+hidecompleted.addEventListener(`click`, function (e) {
 
+    const completedCheckboxes = Array.from(document.querySelectorAll(`p input[type="checkbox"]`));
+    completedCheckboxes.forEach(function (checkbox) {
 
+     
+        if (e.target.checked && checkbox.checked) {
+            checkbox.parentElement.style.display = `none`;
+        }else {
+            checkbox.parentElement.style.display = `block`;
+        }
+    });
 });
 
 
